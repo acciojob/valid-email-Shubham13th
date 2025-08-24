297c9307ccb5513005bff7ec155859d073e4176f
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-24 12:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	if(!str.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}${/){
+		return false;
+	}
+	return true;
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
